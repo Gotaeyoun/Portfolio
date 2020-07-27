c75448c06b757b011521d4576e774e07cc1bff98
Gotaeyoun <ohdin486@gmail.com>
2020-07-27 17:39:14 +0900

    공부중

---
+++ work/js/index.js
@@ -0,0 +1,15 @@
+
+/*  배경 grain */
+var options = {
+  "animate": true,
+  "patternWidth": 100,
+  "patternHeight": 100,
+  "grainOpacity": 0.05,
+  "grainDensity": 1,
+  "grainWidth": 1,
+  "grainHeight": 1
+}
+grained("#wrapper", options);
+
+
+/* 움직이고 색상변경 상자 */
