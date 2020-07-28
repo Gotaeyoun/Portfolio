4557fb44f333ffe40308706c91162cc758fecbee
Gotaeyoun <ohdin486@gmail.com>
2020-07-28 18:27:41 +0900

    완료

--- work/js/index.js
+++ work/js/index.js
@@ -2,14 +2,16 @@
 /*  배경 grain */
 var options = {
   "animate": true,
-  "patternWidth": 100,
+  "patternWidth": 1000,
   "patternHeight": 100,
   "grainOpacity": 0.05,
   "grainDensity": 1,
-  "grainWidth": 1,
+  "grainWidth": 2,
   "grainHeight": 1
 }
 grained("#wrapper", options);
 
 
 /* 움직이고 색상변경 상자 */
+
+
