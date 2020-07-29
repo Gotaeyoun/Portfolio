9fec8188e6188d7200fc69ee3f00bdcc597de720
Gotaeyoun <ohdin486@gmail.com>
2020-07-29 15:50:38 +0900

    연습중

--- work/js/index.js
+++ work/js/index.js
@@ -1,4 +1,3 @@
-
 /*  배경 grain */
 var options = {
   "animate": true,
@@ -13,5 +12,27 @@ grained("#wrapper", options);
 
 
 /* 움직이고 색상변경 상자 */
+var interval = setInterval(function(){
+  $(".move-box").each(function() {
+    var rnd = Math.random() * 20;
+    var rgb = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    $(this).css("height", rnd + "%");
+    $(this).css("background-color", rgb);
+  });
+}, 300);
 
 
+
+
+$('.navi').mouseover(function() {
+  $('.move-box').css("height", "100vh");
+});
+
+
+
+
+// $('#test').hover(function() {
+//   $(this).css("color", "red");
+// }, function(){
+//   $(this).css("color", "blue");
+// });
